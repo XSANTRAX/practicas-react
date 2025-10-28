85610331313d485fe2ddd5a857923af7c736b0eb
Santiago Poveda <santypoveda1112@gmail.com>
2025-10-28 10:13:48 -0500

    Configuración para navegar a través de las diferentes interfaces

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { Navigate } from 'react-router-dom' 
-import Inicio from './Componentes/inicio/Inicio'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Inicio from './Componentes/inicio/Inicio';
+import Empleos from './Componentes/empleos/Empleo';
 
 function App() {
   return (
-    <navigator>
-      <Navigate to="/" element={<Inicio />} />
-      <Navigate to="/Inicio" element={<Inicio />} />
-    </navigator>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Empleos />} />
+        <Route path="/Inicio" element={<Inicio />} />
+        <Route path="/Empleo" element={<Empleos />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
